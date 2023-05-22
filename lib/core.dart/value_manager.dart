f1a3e1bfa2f6bea82361c36c6b59cd54906bd205
Shishir Rijal <ccrrizal0892@gmail.com>
2023-05-22 23:28:19 +0545

    added value manager

---
+++ lib/core.dart/value_manager.dart
@@ -0,0 +1,33 @@
+class AppMargin {
+  static const double m8 = 8.0;
+  static const double m10 = 10.0;
+  static const double m15 = 15.0;
+  static const double m20 = 20.0;
+}
+
+class AppPadding {
+  static const double p8 = 8.0;
+  static const double p10 = 10.0;
+  static const double p15 = 15.0;
+  static const double p20 = 20.0;
+  static const double p28 = 28.0;
+  static const double p100 = 100.0;
+}
+
+class AppSize {
+  static const double s1_5 = 1.5;
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double s10 = 10.0;
+  static const double s15 = 15.0;
+  static const double s20 = 20.0;
+  static const double s28 = 28.0;
+  static const double s40 = 40.0;
+  static const double s80 = 80.0;
+  static const double s100 = 100.0;
+  static const double s200 = 200.0;
+}
+
+class Delay {
+  static const Duration d300 = Duration(milliseconds: 300);
+}
