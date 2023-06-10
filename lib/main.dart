838dcc001e91a56aacca83785bb4947ceafcf244
Shishir Rijal <ccrrizal0892@gmail.com>
2023-06-10 17:17:09 +0545

    updated main with localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:ecommerce_app/app/app.dart';
 import 'package:ecommerce_app/app/di.dart';
+import 'package:ecommerce_app/core.dart/core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await setup();
   runApp(
-    MyApp(),
+    EasyLocalization(
+      supportedLocales: const [englishLocal, nepaliLocal],
+      path: localizationAssetsPath,
+      child: Phoenix(
+        child: MyApp(),
+      ),
+    ),
   );
 }
