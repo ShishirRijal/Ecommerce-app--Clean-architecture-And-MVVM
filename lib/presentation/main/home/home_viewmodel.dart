8f55ad99214922e43b8064c3a4cc280dfa618cde
Shishir Rijal <ccrrizal0892@gmail.com>
2023-06-10 09:26:27 +0545

    added homeviewmodel inputs outputs and added their implementation

--- lib/presentation/main/home/home_viewmodel.dart
+++ lib/presentation/main/home/home_viewmodel.dart
@@ -6,7 +6,8 @@ import 'package:rxdart/subjects.dart';
 import '../../../domain/model/home_object.dart';
 import '../../../domain/usecases/home_usecase.dart';
 
-class HomeViewModel extends BaseViewModel {
+class HomeViewModel extends BaseViewModel
+    implements HomeViewModelInputs, HomeViewModelOutputs {
   final HomeUseCase _useCase;
   HomeViewModel(this._useCase);
 
@@ -25,4 +26,47 @@ class HomeViewModel extends BaseViewModel {
     _storesStreamController.close();
     super.dispose();
   }
+
+  // ! Inputs
+  @override
+  getHome() {
+    // TODO: implement getHome
+    throw UnimplementedError();
+  }
+
+  @override
+  Sink get inputBanners => _bannersStreamController.sink;
+
+  @override
+  Sink get inputServices => _servicesStreamController.sink;
+
+  @override
+  Sink get inputStores => _storesStreamController.sink;
+
+  // ! Outputs
+  @override
+  Stream<List<Banner>> get outputBanners =>
+      _bannersStreamController.stream.map((banners) => banners);
+
+  @override
+  Stream<List<Service>> get outputServices =>
+      _servicesStreamController.stream.map((services) => services);
+
+  @override
+  Stream<List<Store>> get outputStores =>
+      _storesStreamController.stream.map((stores) => stores);
+}
+
+abstract class HomeViewModelInputs {
+  getHome();
+
+  Sink get inputServices;
+  Sink get inputBanners;
+  Sink get inputStores;
+}
+
+abstract class HomeViewModelOutputs {
+  Stream<List<Store>> get outputStores;
+  Stream<List<Service>> get outputServices;
+  Stream<List<Banner>> get outputBanners;
 }
