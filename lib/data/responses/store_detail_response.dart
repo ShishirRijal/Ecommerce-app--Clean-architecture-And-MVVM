b2cc39838a126e55490361122e1f210e178cdfbd
Shishir Rijal <ccrrizal0892@gmail.com>
2023-06-10 13:36:31 +0545

    added store detail response

---
+++ lib/data/responses/store_detail_response.dart
@@ -0,0 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'base_response.dart';
+part 'store_detail_response.g.dart';
+
+@JsonSerializable()
+class StoreDetailsResponse extends BaseResponse {
+  @JsonKey(name: 'id')
+  int? id;
+  @JsonKey(name: 'title')
+  String? title;
+  @JsonKey(name: 'image')
+  String? image;
+  @JsonKey(name: 'details')
+  String? details;
+  @JsonKey(name: 'services')
+  String? services;
+  @JsonKey(name: 'about')
+  String? about;
+
+  StoreDetailsResponse(
+      this.id, this.title, this.image, this.details, this.services, this.about);
+
+  factory StoreDetailsResponse.fromJson(Map<String, dynamic> json) =>
+      _$StoreDetailsResponseFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => _$StoreDetailsResponseToJson(this);
+}
